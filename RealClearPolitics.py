2d6c74a145b774dff52b46cdd00d65ccb01291c2
alvinlee123 <Alvinlee123@hotmail.com>
2014-04-11 04:01:19 -0700

    Modified class def for RCP

--- RealClearPolitics.py
+++ RealClearPolitics.py
@@ -33,13 +33,4 @@ def plot_colors(xml):
     return result
 
 def rcp_poll_data(xml):
-    
-    
-    
-    
-    touch README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/alvinlee123/Webscraping.git
-git push -u origin master
+    print hi
